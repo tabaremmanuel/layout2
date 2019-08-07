b4d4bd130ecd548135a8caeecf2a1b7a066891bd
Leejee Moody Zamora <leejeemoodyzamora@Leejees-MacBook-Air.local>
2019-08-07 21:07:17 +0800

    css

--- wp-content/themes/layout2-2019-child/header.php
+++ wp-content/themes/layout2-2019-child/header.php
@@ -9,3 +9,33 @@
 
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
+
+<div class="nav-top">
+	<div class="container">
+		<div class="row">
+			<div class="col-3">
+				<a href="#">
+					<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo.png" alt="Logo">
+				</a>
+			</div>
+			<div class="col-9 text-right">
+				<div class="main-menu">
+					<ul>
+						<li>
+							<a href="#">Menu 1</a>
+						</li>
+						<li>
+							<a href="#">Menu 2</a>
+						</li>
+						<li>
+							<a href="#">Menu 3</a>
+						</li>
+						<li>
+							<a href="#">Menu 4</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
--- wp-content/themes/layout2-2019-child/page-home.php
+++ wp-content/themes/layout2-2019-child/page-home.php
@@ -1,3 +1,18 @@
 <?php /*Template Name: Home*/ get_header(); ?>
-  
+  <div class="section section-1">
+    <a href="javascript:void(0)" class="btn-arr-down w-100 position-absolute text-center p-4">
+      <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/btn-arr-down.png" alt="">
+    </a>
+    <div class="container">
+      <div class="row align-content-center">
+        <div class="col-md-8 offset-md-2 text-center">
+          <h2>Sample Text</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="section section-2">
+
+  </div>
 <?php get_footer();
